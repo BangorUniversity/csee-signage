c8611a640ad4649bf290cb4cb013f2ec315ccb62
Joseph Mearman <joseph@mearman.co.uk>
2020-02-18 09:08:52 +0000

    create install_cron script

--- first-run.sh
+++ first-run.sh
@@ -12,17 +12,7 @@ sudo apt -y autoremove
 
 sudo apt -y install xdotool unclutter sed git
 
-#sudo crontab -l > temp_cron
-
-#echo new cron into cron file
-echo "0 8 * * 1-5 /opt/vc/bin/tvservice -p && chvt 6 && chvt 7" >> temp_cron
-echo "0 18 * * * /opt/vc/bin/tvservice -o" >> temp_cron
-echo "0 7 * * 1 root (apt -y update && apt -y upgrade) > /dev/null && /sbin/shutdown -r now" >> temp_cron
-sudo echo "30 7 * * 1-5 \$(cd /home/pi/scsee-signage/ && git pull && sudo bash update.sh)" >> temp_cron
-
-#install new cron file
-sudo crontab temp_cron
-rm temp_cron
+sudo bash install_cron.sh
 
 cd /home/pi/scsee-signage/
 git clone https://github.com/BangorUniversity/scsee-signage
--- update.sh
+++ update.sh
@@ -3,3 +3,5 @@ sudo systemctl stop kiosk.service
 sudo systemctl disable kiosk.service
 sudo cp kiosk.service /lib/systemd/system/kiosk.service
 sudo systemctl enable kiosk.service
+
+sudo bash install_cron.sh
