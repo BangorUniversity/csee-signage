075a37538249c0d1db13d082a89e61ce46baa861
Joseph Mearman <joseph@mearman.co.uk>
2020-02-18 09:51:52 +0000

    add cec utils to first-run

--- first-run.sh
+++ first-run.sh
@@ -10,18 +10,10 @@ sudo apt -y purge smartsim java-common minecraft-pi libreoffice* -y
 sudo apt clean
 sudo apt -y autoremove
 
-sudo apt -y install xdotool unclutter sed git
-
-sudo bash install_cron.sh
+sudo apt -y install xdotool unclutter sed git libcec3 cec-utils
 
 cd /home/pi/scsee-signage/
-git clone https://github.com/BangorUniversity/scsee-signage
-cd scsee-signage
-
-sudo chown 777 kiosk.sh
-sudo cp kiosk.service /lib/systemd/system/kiosk.service
-
-sudo systemctl enable kiosk.service
+sudo bash update.sh
 
 sudo bash /usr/lib/raspi-config/init_resize.sh
 
