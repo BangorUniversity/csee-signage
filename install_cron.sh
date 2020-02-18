d30bb55a04f9ff878906aef7ccaa05fdcbe725d7
Joseph Mearman <joseph@mearman.co.uk>
2020-02-18 15:40:53 +0000

    fix typo

--- install_cron.sh
+++ install_cron.sh
@@ -4,7 +4,7 @@
 
 #echo new cron into cron file
 
-echo "@reboot \$(cd /home/pi/scsee-signage && git sudo bash update.sh; echo 'on 0' | cec-client -s -d 1)" >> temp_cron
+echo "@reboot \$(cd /home/pi/scsee-signage && sudo bash update.sh; echo 'on 0' | cec-client -s -d 1)" >> temp_cron
 echo "59 7 * * 1-5 echo 'on 0' | cec-client -s -d 1" >> temp_cron
 echo "0 18 * * * echo 'standby 0' | cec-client -s -d 1 && echo 'as' | cec-client -s -d 1" >> temp_cron
 echo "0 7 * * 1 root (apt -y update && apt -y upgrade) > /dev/null && /sbin/shutdown -r now" >> temp_cron
