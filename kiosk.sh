c7f271770a9753ed24c9065457ab028eff28b98e
Joseph Mearman <joseph@mearman.co.uk>
2020-02-18 10:06:10 +0000

    reduce kiosk delay

--- kiosk.sh
+++ kiosk.sh
@@ -8,7 +8,8 @@ unclutter -idle 0.5 -root &
 
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
-sleep 60 
+
+sleep 15 
 
 /usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk https://bit.ly/csee-foyer &
 
