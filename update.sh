31f14c3369e6a88f27368b6620a161772167d111
Joseph Mearman <joseph@mearman.co.uk>
2020-02-19 08:34:01 +0000

    forgot to restart service in update

--- update.sh
+++ update.sh
@@ -5,7 +5,10 @@ git reset --hard origin/master
 
 sudo systemctl stop kiosk.service
 sudo systemctl disable kiosk.service
+
 sudo cp kiosk.service /lib/systemd/system/kiosk.service
+
 sudo systemctl enable kiosk.service
+sudo systemctl start kiosk.service
 
 sudo bash install_cron.sh
