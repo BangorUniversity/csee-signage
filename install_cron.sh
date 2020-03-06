dcdc3712d5f1f831e95f3767052260d8f290896e
Joseph Mearman <joseph@mearman.co.uk>
2020-03-06 12:02:33 +0000

    tweak cron

--- install_cron.sh
+++ install_cron.sh
@@ -2,14 +2,12 @@
 
 #sudo crontab -l > temp_cron
 
-#echo new cron into cron file
+echo "0 8 * * 2-5 /sbin/shutdown -r now" >> temp_cron
+echo "0 8 * * 1 root (apt -y update && apt -y upgrade) > /dev/null && /sbin/shutdown -r now" >> temp_cron
 
-echo "@reboot sudo bash regular-update.sh; echo 'on 0' | cec-client -s -d 1" >> temp_cron
-echo "59 7 * * 1-5 echo 'on 0' | cec-client -s -d 1" >> temp_cron
-echo "0 18 * * * echo 'standby 0' | cec-client -s -d 1 && echo 'as' | cec-client -s -d 1" >> temp_cron
-echo "0 7 * * 1 root (apt -y update && apt -y upgrade) > /dev/null && /sbin/shutdown -r now" >> temp_cron
-echo "30 7 * * 1-5 sudo bash regular-update.sh; echo 'on 0' | cec-client -s -d 1" >> temp_cron
+echo "@reboot sudo bash /home/pi/scsee-signage/regular-update.sh; /opt/vc/bin/tvservice -p && chvt 6 && chvt 7; echo 'on 0' | cec-client -s -d 1" >> temp_cron
 
-#install new cron file
+echo "0 18 * * * echo 'standby 0' | cec-client -s -d 1 && echo 'as' | cec-client -s -d 1; /opt/vc/bin/tvservice -o" >> temp_cron
+
 sudo crontab temp_cron
 rm temp_cron
